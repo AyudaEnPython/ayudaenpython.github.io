3cb1e3c759a7ce894ab966701c3e399e66e611d2
leugimkm <leugim911@hotmail.com>
2021-11-04 12:00:29 -0500

    update

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,37 @@
+var store = [{
+        "title": "Bienvenidos!",
+        "excerpt":"def saludar(nombre: str) -&gt; str: \"\"\"Saludo de bienvenida a los integrantes de la comunidad 'Ayuda en Python'. :param nombre: Nombre de usuario :type nombre: str :return: Mensaje de bienvenida :rtype: str \"\"\" return f\"Hola {nombre.capitalize()}, \" \\ \"'Ayuda en Python' te da la bienvenida!\" if __name__ == \"__main__\": nombre =...","categories": [],
+        "tags": [],
+        "url": "/bienvenido/",
+        "teaser": null
+      },{
+        "title": "Variables",
+        "excerpt":"Definiciones Variables Un variable conceptualmente, es un contenedor etiquetado el cual almacena un valor. Para una definición más detallada puedes pulsar aquí. Identificador Un identificador es un nombre que puede darse a una variable, función, clase, módulo u otro objeto (en Python todo es un objeto). Los identificadores empiezan con...","categories": [],
+        "tags": [],
+        "url": "/basico/",
+        "teaser": null
+      },{
+        "title": "Generar números aleatorios",
+        "excerpt":"Test  ","categories": [],
+        "tags": [],
+        "url": "/revisiones/generar-numeros-aleatorios/",
+        "teaser": null
+      },{
+        "title": "Introducción",
+        "excerpt":"¿Qué es Python? Python es un lenguaje de programación interpretado cuya filosofía hace hincapié en la legibilidad de su código. Se trata de un lenguaje de programación multiparadigma, ya que soporta parcialmente la orientación a objetos, programación imperativa y, en menor medida, programación funcional. Es un lenguaje interpretado, dinámico y...","categories": [],
+        "tags": [],
+        "url": "/tutoriales/introduccion/",
+        "teaser": null
+      },{
+        "title": "Python es de tipado dinámico y fuerte",
+        "excerpt":"Python efectivamente es fuertemente tipado y dinámico a la vez, para ello vamos a analizarlo por partes: Tipado dinámico Un lenguaje de programación usa un tipado dinámico cuando la comprobación de tipificación (typing) se realiza durante su ejecución y no en su compilación. &gt;&gt;&gt; numero = 1 &gt;&gt;&gt; print(numero) 1...","categories": [],
+        "tags": [],
+        "url": "/tutoriales/tipado-dinamico-y-fuerte/",
+        "teaser": null
+      },{
+        "title": "Estructuras de Datos",
+        "excerpt":"Listas (list) Es una secuencia ordenada y mutable de elementos (diferentes tipos) usadas para almacenar colecciones de elementos homogéneos. &gt;&gt;&gt; pares = [2, 4, 6, 8] print(pares) [2, 4, 6, 8] Tuplas (tuple) Son secuencias inmutables usadas normalmente para almacenar colecciones de datos heterogéneos. &gt;&gt;&gt; vocales = ('a', 'e', 'i',...","categories": [],
+        "tags": [],
+        "url": "/tutoriales/estructuras-de-datos/",
+        "teaser": null
+      }]
