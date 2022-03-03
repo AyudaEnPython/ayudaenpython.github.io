db6cdfdd69ee3ff393cec1a2f6b8636092a5c322
leugimkm <leugim911@hotmail.com>
2022-03-02 22:05:57 -0500

    update

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,26 +1,8 @@
 var store = [{
-        "title": "Bienvenidos a nuestro proyecto AyudaEnPython!",
+        "title": "Bienvenidos a nuestro proyecto Ayuda en Python!",
         "excerpt":"Bienvenidos def saludar(nombre: str) -&gt; str: \"\"\"Saludo de bienvenida a los integrantes de la comunidad 'Ayuda en Python'. :param nombre: Nombre de usuario :type nombre: str :return: Mensaje de bienvenida :rtype: str \"\"\" return f\"Hola {nombre.capitalize()}, \" \\ \"'Ayuda en Python' te da la bienvenida!\" if __name__ == \"__main__\": nombre...","categories": [],
         "tags": [],
         "url": "/bienvenido/",
-        "teaser": null
-      },{
-        "title": "Generar números aleatorios",
-        "excerpt":"Test  ","categories": [],
-        "tags": [],
-        "url": "/revisiones/generar-numeros-aleatorios/",
-        "teaser": null
-      },{
-        "title": "Introducción",
-        "excerpt":"¿Qué es Python? Python es un lenguaje de programación interpretado cuya filosofía hace hincapié en la legibilidad de su código. Se trata de un lenguaje de programación multiparadigma, ya que soporta parcialmente la orientación a objetos, programación imperativa y, en menor medida, programación funcional. Es un lenguaje interpretado, dinámico y...","categories": [],
-        "tags": [],
-        "url": "/tutoriales/introduccion/",
-        "teaser": null
-      },{
-        "title": "Instalación",
-        "excerpt":"Instalación Instalar en Windows Primero seguir las instrucciones de instalación para Chocolatey (gestor de paquetes al estilo de apt-get): Nota: Copiar y ejecutar el siguiente comando en PowerShell Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) Una vez instaldo Chocolatey, instalar Python es sencillo: choco...","categories": [],
-        "tags": [],
-        "url": "/tutoriales/instalacion/",
         "teaser": null
       },{
         "title": "Python es de tipado dinámico y fuerte",
@@ -29,21 +11,57 @@ var store = [{
         "url": "/tutoriales/tipado-dinamico-y-fuerte/",
         "teaser": null
       },{
-        "title": "Estructuras de Datos",
-        "excerpt":"Una estructura de datos es una forma particular de organizar datos en una computadora para que puedan ser utilizados de manera eficiente. 1 Python ayuda a aprender los fundamentos de estas estructuras de una manera simple comparada a otros lenguajes de programación, ofreciendo en su documentación oficial varios ejemplos. 2...","categories": [],
+        "title": "Generar números aleatorios",
+        "excerpt":"Código original: import random def aleat(datos): suma=0 for n in datos: suma +=n return suma datos=[] felo= random.randint(1, 20) print(\"El tamaño de la lista es: \",felo ) for n in range(felo): crew= random.randint(1, 100) datos.append(crew) #datos= tuple(datos) print(datos) SumaT= aleat(datos) print(\"La suma total e s: \", SumaT) Código refactorizado: from...","categories": [],
         "tags": [],
-        "url": "/tutoriales/estructuras-de-datos/",
+        "url": "/revisiones/generar-numeros-aleatorios/",
+        "teaser": null
+      },{
+        "title": "Introducción",
+        "excerpt":"¿Qué es Python? Python es un lenguaje de programación interpretado cuya filosofía hace hincapié en la legibilidad de su código. Se trata de un lenguaje de programación multiparadigma, ya que soporta parcialmente la orientación a objetos, programación imperativa y, en menor medida, programación funcional. Es un lenguaje interpretado, dinámico y...","categories": [],
+        "tags": [],
+        "url": "/tutorial-python/introduccion/",
+        "teaser": null
+      },{
+        "title": "Instalación",
+        "excerpt":"Instalación Instalar en Windows Primero seguir las instrucciones de instalación para Chocolatey (gestor de paquetes al estilo de apt-get): Nota: Copiar y ejecutar el siguiente comando en PowerShell Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) Una vez instaldo Chocolatey, instalar Python es sencillo: choco...","categories": [],
+        "tags": [],
+        "url": "/tutorial-python/instalacion/",
         "teaser": null
       },{
         "title": "Conceptos Básicos",
         "excerpt":"Variable Un variable conceptualmente es un contenedor etiquetado el cual almacena un valor 1. Para una definición más detallada puedes pulsar aquí. Ejemplo: &gt;&gt;&gt; nombre = \"John\" &gt;&gt;&gt; edad = 18 La variable nombre almacena el valor \"John\" (cadena de texto) y la variable edad almacena el valor 18 (entero)....","categories": [],
         "tags": [],
-        "url": "/tutoriales/variables/",
+        "url": "/tutorial-python/conceptos-basicos/",
         "teaser": null
       },{
-        "title": "Espacios de nombre y alcance",
-        "excerpt":"Entender bien los espacios de nombre y su alcance en Python permiten mejorar la legibilidad del código. Espacios de nombre (namepaces) Existen 4 tipos de espacios de nombre Built-in Global Enclosing Local Buenas prácticas Por lo general en cualquier lenguaje de programación no se aconseja modificar variables globales. Cuando una...","categories": [],
+        "title": "Estructuras de Datos",
+        "excerpt":"Una estructura de datos es una forma particular de organizar datos en una computadora para que puedan ser utilizados de manera eficiente. 1 Python ayuda a aprender los fundamentos de estas estructuras de una manera simple comparada a otros lenguajes de programación, ofreciendo en su documentación oficial varios ejemplos. 2...","categories": [],
         "tags": [],
-        "url": "/tutoriales/namespace/",
+        "url": "/tutorial-python/estructuras-de-datos/",
+        "teaser": null
+      },{
+        "title": "Estructuras de Control Condicionales",
+        "excerpt":"Definición   Las estructuras condicionales son aquellas que nos permiten evaluar si una o más condiciones se cumplen para decir que acción vamos a ejecutar. La evaluación de condiciones solo puede arrojar los resultados verdadero o falso (True o False).   En progreso…  ","categories": [],
+        "tags": [],
+        "url": "/tutorial-python/condicionales/",
+        "teaser": null
+      },{
+        "title": "Estructuras de Control Iterativas",
+        "excerpt":"Definición   A diferencia de las estructuras de control condicionales, las iterativas (también llamadas cíclicas o bucles) nos permiten ejecutar un mismo código de manera repetida mientras se cumpla una condición.   En progreso…  ","categories": [],
+        "tags": [],
+        "url": "/tutorial-python/iterativas/",
+        "teaser": null
+      },{
+        "title": "Funciones",
+        "excerpt":"Definición   Una función es la forma de agrupar expresiones y sentencias (algoritmos) que realizan determinadas acciones, pero que estas solo se ejecutan cuando son llamadas.   En progreso…  ","categories": [],
+        "tags": [],
+        "url": "/tutorial-python/funciones/",
+        "teaser": null
+      },{
+        "title": "Listas de Comprensión",
+        "excerpt":"En progreso…  ","categories": [],
+        "tags": [],
+        "url": "/tutoriales/listas-de-comprension/",
         "teaser": null
       }]
